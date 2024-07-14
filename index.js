7b4236c5c7b1c4949d199a558cb9b273ea5277d3
Bereket <bereketgirma028@gmail.com>
2024-07-14 17:05:00 -0400

    Added SVM

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("knnRadio").addEventListener("change", updateVisualizer);
+document.getElementById("svmRadio").addEventListener("change", updateVisualizer);
+
+function updateVisualizer() {
+    const visualizer = document.getElementById("visualizer");
+    const description = document.getElementById("description");
+
+    if (document.getElementById("knnRadio").checked) {
+        visualizer.src = "knn.html";
+        description.innerText = "This visualizer allows you to generate random data points and run the K-Nearest Neighbors (K-NN) algorithm to classify a target point. You can move points around to see how the nearest neighbors change in real-time.";
+    } else if (document.getElementById("svmRadio").checked) {
+        visualizer.src = "svm.html";
+        description.innerText = "This visualizer allows you to generate random data points and run the Support Vector Machine (SVM) algorithm to classify a target point. You can move points around to see how the separating hyperplane changes in real-time.";
+    }
+}
